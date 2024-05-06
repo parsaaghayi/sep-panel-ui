218c9c8b726a2cb2d3ed1d07a2cc8d9866ea5030
parsa aghaei <p.aghaei@hoshmand.local>
2024-05-06 11:41:14 +0330

    modal updated

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { EventHandler, useEffect, useState } from "react";
 import "./style.css";
 
 import warning from "./../../images/warning.svg";
@@ -10,6 +10,7 @@ type ModalPropsType = {
   type?: "base" | "warning" | "danger";
   showModal: boolean;
   children: React.ReactElement;
+  onClickOutClose?: boolean;
   submitButtonLabel?: string;
   cancelButtonLabel?: string;
 } & (
@@ -24,43 +25,76 @@ const Modal: React.FC<ModalPropsType> = ({
   children,
   submitButtonLabel,
   cancelButtonLabel,
+  onClickOutClose = cancelButtonLabel === undefined || cancelButtonLabel === ""
+    ? true
+    : false,
   onSubmit,
 }) => {
   const [showModalState, setshowModalState] = useState(showModal);
+  const [isOpen, setIsOpen] = useState(showModal);
+  useEffect(() => {
+    if (!showModalState) {
+      setTimeout(() => {
+        setIsOpen(false);
+      }, 300);
+    }
+  }, [showModalState]);
+
+  function handleModalClick(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    const target = e.target as HTMLElement;
+    if (target.classList.contains("modal-container")) {
+      setshowModalState(false);
+    }
+  }
+
   return (
     <>
-      {showModalState ? (
-        <div className="modal-body">
-          <h2 className="modal-title">
-            {type === "warning" || type === "danger" ? (
-              <img
-                src={type === "warning" ? warning : error}
-                alt={type === "warning" ? "warning icon" : "danger icon"}
-              />
-            ) : (
-              <></>
-            )}
-            {title}
-          </h2>
-          <div className="modal-description">{children}</div>
-          <div className="modal-footer">
-            <Button
-              label={cancelButtonLabel ? cancelButtonLabel : "انصراف"}
-              type="subtle"
-              onClick={() => setshowModalState(false)}
-            />
-            {submitButtonLabel ? (
-              <Button
-                label={submitButtonLabel}
-                type={
-                  type === "danger"
-                    ? "danger"
-                    : type === "warning"
-                      ? "warning"
-                      : "primary"
-                }
-                onClick={() => onSubmit()}
-              />
+      {console.log(cancelButtonLabel)}
+      {isOpen ? (
+        <div
+          className={`modal-container ${showModalState ? "show" : ""}`}
+          onClick={(e) => {
+            if (onClickOutClose) {
+              handleModalClick(e);
+            }
+          }}
+        >
+          <div className="modal-body">
+            <h2 className="modal-title">
+              {type === "warning" || type === "danger" ? (
+                <img
+                  src={type === "warning" ? warning : error}
+                  alt={type === "warning" ? "warning icon" : "danger icon"}
+                />
+              ) : (
+                <></>
+              )}
+              {title}
+            </h2>
+            <div className="modal-description">{children}</div>
+            {submitButtonLabel || cancelButtonLabel ? (
+              <div className="modal-footer">
+                <Button
+                  label={cancelButtonLabel ? cancelButtonLabel : "انصراف"}
+                  type="subtle"
+                  onClick={() => setshowModalState(false)}
+                />
+                {submitButtonLabel ? (
+                  <Button
+                    label={submitButtonLabel}
+                    type={
+                      type === "danger"
+                        ? "danger"
+                        : type === "warning"
+                          ? "warning"
+                          : "primary"
+                    }
+                    onClick={() => onSubmit()}
+                  />
+                ) : (
+                  <></>
+                )}
+              </div>
             ) : (
               <></>
             )}
