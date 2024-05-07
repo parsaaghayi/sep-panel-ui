5e00a95d6094f1ff57a7ea9aec645a70994667b3
parsa aghaei <p.aghaei@hoshmand.local>
2024-05-07 17:40:21 +0330

    DropDownMenu added

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Avatar } from "./Avatar";
 export { default as Button } from "./Button";
 export { default as Checkbox } from "./Checkbox";
+export { default as DropDownMenu } from "./DropDownMenu";
 export { default as Modal } from "./Modal";
 export { default as Pagination } from "./Pagination";
 export { default as Tabs } from "./Tabs";
