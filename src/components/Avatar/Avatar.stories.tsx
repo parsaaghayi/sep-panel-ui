3746174ca463976a7c6d7169965a5213457b06a8
parsa aghaei <p.aghaei@hoshmand.local>
2024-05-07 10:31:27 +0330

    avatar added

--- src/components/Avatar/Avatar.stories.tsx
+++ src/components/Avatar/Avatar.stories.tsx
@@ -1,22 +1,13 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Avatar from "./Avatar";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "sep-panel-ui/Avatar",
   component: Avatar,
-  //   parameters: {
-  //     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-  //     layout: 'centered',
-  //   },
 } satisfies Meta<typeof Avatar>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
-
-function onClickFunction() {
-  console.log("this is Avatar onClick function");
-}
 
 export const icon: Story = {
   args: {
