2bbaf0af6e21b60d4f9cc57d605ca1ae5c33a916
parsa aghaei <p.aghaei@hoshmand.local>
2024-05-11 13:29:57 +0330

    Progressbar added

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,5 +5,6 @@ export { default as Checkbox } from "./Checkbox";
 export { default as DropDownMenu } from "./DropDownMenu";
 export { default as Modal } from "./Modal";
 export { default as Pagination } from "./Pagination";
+export { default as Progressbar } from "./Progressbar";
 export { default as Tabs } from "./Tabs";
 export { default as Toggle } from "./Toggle";
