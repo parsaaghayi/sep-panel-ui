826c805f64ea217caa95aa46c6375c95156c2c27
parsa aghaei <p.aghaei@hoshmand.local>
2024-05-14 11:26:57 +0330

    RadioGroup added

--- src/components/RadioGroup/RadioGroup.test.tsx
+++ src/components/RadioGroup/RadioGroup.test.tsx
@@ -6,10 +6,64 @@ describe("Radio", () => {
   test("render the Radio component", () => {
     render(
       <Radio
-        checked={true}
+        title="test radio group"
+        options={[
+          {
+            label: "label 1",
+            value: "label1",
+            id: "label1",
+          },
+          {
+            label: "label 2",
+            value: "label2",
+            id: "label2",
+            disabled: true,
+          },
+          {
+            label: "label 3",
+            value: 3,
+            id: "label3",
+          },
+          {
+            label: "label 4",
+            value: 4,
+            id: "label4",
+          },
+          {
+            label: "label 5",
+            value: 5,
+            id: "label5",
+          },
+          {
+            label: "label 6",
+            value: 6,
+            id: "label6",
+          },
+          {
+            label: "label 7",
+            value: 7,
+            id: "label7",
+          },
+          {
+            label: "label 8",
+            value: 8,
+            id: "label8",
+          },
+          {
+            label: "label 9",
+            value: 9,
+            id: "label9",
+          },
+          {
+            label: "label 10",
+            value: 10,
+            id: "label10",
+          },
+        ]}
+        selectedOptionValue="label1"
         name="test"
-        id="test"
-        onChange={() => console.log("changed")}
+        required={true}
+        onChange={(value) => console.log(value)}
       />
     );
   });
