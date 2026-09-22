f700e4d94fd3854669ab783427592038109f7672
parsa aghaei <p.aghaei@hoshmand.local>
2026-09-22 10:16:43 +0330

    chore(storybook): upgrade to Storybook v10.6.0 (dev tooling only)

--- src/components/DropDownMenu/DropDownMenu.stories.tsx
+++ src/components/DropDownMenu/DropDownMenu.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-webpack5";
 import DropDownMenu from "./DropDownMenu";
 import React from "react";
 
